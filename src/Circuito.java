a7ad436ee09349531aedb66824888e0a6b4bf764
Marcos <cernadascurrante9399@hotmail.com>
2026-02-23 10:09:22 +0100

    Getters y Setters clase Circuito

--- src/Circuito.java
+++ src/Circuito.java
@@ -1,4 +1,18 @@
 public class Circuito {
     private String nombre;
     private int longitud;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
 }
